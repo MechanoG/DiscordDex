98b92cc97054eb0a1ee82c8542d3ea868f1ec6bd
MechanoG <glory_to_the_machine@protonmail.com>
2025-08-09 01:07:26 -0400

    Pone mensaje de inicio.

---
+++ index.js
@@ -0,0 +1,12 @@
+const {Client, Events, GatewayIntentBits} = require(`discord.js`);
+const { token } = require(`./config.json`);
+
+//New client instance
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, readyClient =>{
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+//Log in to disconr whit your client tokenb
+client.login(token);
