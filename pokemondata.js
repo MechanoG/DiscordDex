162e789e3581efdb2d49ab8c36b2ca02a227af63
MechanoG <glory_to_the_machine@protonmail.com>
2025-08-21 00:16:57 -0400

    Put the bot on spanish and organice data

--- pokemondata.js
+++ pokemondata.js
@@ -66,9 +66,32 @@ function statsReturn(stats){
     let finalStats = [];
 
     for (let element of stats){
-        const statName = element.stat.name;
+
         const statValue = element.base_stat;
-        finalStats.push([statName, statValue]);
+        const statName = element.stat.name;
+        switch(statName){
+            
+            case 'hp':
+                finalStats.push(['Hp', statValue]);        
+                break;
+            case 'attack':
+                finalStats.push(['Ataque', statValue]);        
+                break;
+            case 'defense':
+                finalStats.push(['Defensa', statValue]);        
+                break;
+            case 'special-attack':
+                finalStats.push(['Ataque especial', statValue]);        
+                break;
+            case 'special-defense':
+                finalStats.push(['Defensa especial', statValue]);        
+                break;
+            case 'speed':
+                finalStats.push(['Velocidad', statValue]);        
+                break;
+            default:
+                finalStats.push([statName, statValue]);
+        }
     }
 
     console.log(finalStats);
@@ -78,21 +101,90 @@ function statsReturn(stats){
 
 function typeAsing(types){
 
-    if (types.length>1) return `${types[0].type.name.slice(0,1).toUpperCase() + types[0].type.name.slice(1)}` + ' | ' + 
-                               `${types[1].type.name.slice(0,1).toUpperCase() + types[1].type.name.slice(1)}`
-    return `${types[0].type.name.slice(0,1).toUpperCase() + types[0].type.name.slice(1)}`  
+    if (types.length>1)    
+        
+        return `${parseType(types[0].type.name)}` + ' | ' + `${parseType(types[1].type.name)}`
+    return `${parseType(types[0].type.name)}`  
+}
+
+function parseType(type){
+    switch(type){
+        case "normal" :
+            return "Normal";
+            break;
+        case "fighting" :
+            return "Lucha";
+            break;
+        case "flying" :
+            return "Volador";
+            break;
+        case "poison" :
+            return "Veneno";
+            break;
+        case "ground" :
+            return "Tierra";
+            break;
+        case "rock" :
+            return "Roca";
+            break;
+        case "bug" :
+            return "Bicho";
+            break;
+        case "ghost" :
+            return "Fantasma";
+            break;
+        case "steel" :
+            return "Acero";
+            break;
+        case "fire" :
+            return "Fuego";
+            break;
+        case "water" :
+            return "Agua";
+            break;
+        case "grass" :
+            return "Planta";
+            break;
+        case "electric" :
+            return "Electrico";
+            break;
+        case "psychic" :
+            return "Psiquico";
+            break;
+        case "ice" :
+            return "Hielo";
+            break;
+        case "dragon" :
+            return "Dragon";
+            break;
+        case "dark" :
+            return "Siniestro";
+            break;
+        case "fairy" :
+            return "Hada";
+            break;
+        case "stellar" :
+            return "Astral";
+            break;
+        case "unknown" :
+            return "???";
+            break;
+        default:
+            return type;
+    }
+
 }
 
 function embedReturn(name, spriteUrl, dexData, types, stats, dexNum, altura, peso){
 
     const pokeEmbed = new EmbedBuilder()
-        .setTitle('NATIONAL POKEDEX:')
+        .setTitle('POKEDEX NACIONAL:')
         .setImage(`${spriteUrl}`)
         .addFields(
-            {name: `No${dexNum}  ${name.slice(0,1).toUpperCase()}${name.slice(1)} `, value: ""},
-            {name: `HEIGHT: ${altura/10} m`, value:""},
-            {name: `WEIGHT: ${peso/10} kg`, value:""},
-            {name: `TYPES: ${typeAsing(types)}`, value:""},
+            {name: `No ${dexNum}  ${name.slice(0,1).toUpperCase()}${name.slice(1)} `, value: ""},
+            {name: `ALTURA: ${altura/10} m`, value:""},
+            {name: `PESO: ${peso/10} kg`, value:""},
+            {name: `TIPOS: ${typeAsing(types)}`, value:""},
             {name: `${dexData}`, value: ""},
             {name: `Estadisticas`, value: `${stats[0][0]} : ${stats[0][1]}
                                     ${stats[1][0]} : ${stats[1][1]} 
